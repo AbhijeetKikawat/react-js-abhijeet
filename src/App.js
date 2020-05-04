cfc4ef50085a84e09749daf4142b56b89305a938
Abhijeet Kikawat <abhijeetkikawat1009@gmail.com>
2020-05-04 18:11:50 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm Abhijeet</h1>
+        <h2>I'm a developer</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
